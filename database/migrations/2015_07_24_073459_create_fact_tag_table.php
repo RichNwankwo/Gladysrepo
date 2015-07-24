be8b79c680956d09b769af42eb1bb578e25903c1
RichNwankwo <RichardNwankwo@visneta.com>
2015-07-24 03:49:10 -0400

    Adds fact - tag pivot table

---
+++ database/migrations/2015_07_24_073459_create_fact_tag_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFactTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creates the pivot table for tags and facts
+        Schema::create('facts_tags', function(Blueprint $table){
+            
+            $table->increments('id')->unsigned();
+            $table->integer('fact_id')->unsigned();
+            $table->foreign('fact_id')->references('id')->on('fact');
+            $table->integer('tag_id')->unsigned();
+            $table->foreign('tag_id')->references('id')->on('tag');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drops the pivot table for tags and facts
+        Schema::drop('facts_tags');
+    }
+}
