e3ab1b603901fb97a2755c215973f7ce4a976a63
RichNwankwo <RichardNwankwo@visneta.com>
2015-08-22 07:07:28 -0400

    Tagged Random Fact feature and test

--- app/GladysApp/Domain/PracticeSessionTools/FactSelector.php
+++ app/GladysApp/Domain/PracticeSessionTools/FactSelector.php
@@ -3,6 +3,7 @@
 
 namespace App\GladysApp\Domain;
 use App\GladysApp\Transformers\FactTransformer;
+use App\Models\TaggedFact;
 use App\Models\User;
 
 class FactSelector implements FactSelectorInterface{
@@ -17,10 +18,32 @@ class FactSelector implements FactSelectorInterface{
     public function selectRandomFact($user_id)
     {
         $facts = User::find($user_id)->facts;
-        if(!$facts){
-            return False;
+        return $this->transformAndReturnFact($facts);
+    }
+
+    public function selectRandomTaggedFact($user_id, $tag_id)
+    {
+        $tagged_facts = TaggedFact::whereRaw("tag_id = $tag_id")->get();
+        foreach($tagged_facts as $t_g)
+        {
+            $facts[] = $t_g->fact;
         }
-        $random_fact = $facts[mt_rand(0, count($facts)-1)];
+        return $this->transformAndReturnFact($facts);
+    }
+
+    /**
+     * @param $facts
+     * @return array|bool
+     */
+    protected function transformAndReturnFact($facts)
+    {
+        if(!$facts)
+        {
+            return false;
+        }
+        $random_fact = $facts[mt_rand(0, count($facts) - 1)];
         return $this->factTransformer->transform($random_fact);
     }
+
+
 }
--- tests/PracticeSessions/FactSelectorTest.php
+++ tests/PracticeSessions/FactSelectorTest.php
@@ -2,6 +2,7 @@
 
 use  App\Models\Fact;
 use App\Models\User;
+use App\Models\Tag;
 class FactSelectorTest extends TestCase{
 
 
@@ -33,6 +34,45 @@ class FactSelectorTest extends TestCase{
         $fact = $this->factSelector->selectRandomFact($user->id);
 
         $this->assertArrayHasKey('user_id', $fact);
+
+    }
+
+    public function test_If_random_tagged_fact_is_selected()
+    {
+        $user = \App\Models\User::create([
+            'name' => 'Test User',
+            'email' => 'testing@testing.com',
+            'password' => Hash::make('testing')
+        ]);
+
+        Fact::insert([
+            ['user_id'=> $user->id, 'fact'=> 'I believe I can fly'],
+            ['user_id'=> $user->id, 'fact'=> 'I provide that new new'],
+            ['user_id'=> $user->id, 'fact'=> 'A love supreme'],
+            ['user_id'=> $user->id, 'fact'=> 'Going back to honolulu'],
+            ['user_id'=> $user->id, 'fact'=> 'Jesus Walks'],
+            ['user_id'=> $user->id, 'fact'=> 'Ride pipe like bike like YOKOHAMA']
+        ]);
+
+        $tag_id = Tag::create(['tag_name'=> 'Test Tag'])->id;
+
+        $facts = Fact::where('user_id', $user->id)->get();
+        foreach($facts as $fact)
+        {
+            \App\Models\TaggedFact::create(['fact_id' => $fact->id, 'tag_id' => $tag_id]);
+        }
+
+        $this->factSelector = new \App\GladysApp\Domain\FactSelector();
+        $fact = $this->factSelector->selectRandomTaggedFact($user->id, $tag_id);
+        $fact_id = $fact['id'];
+
+        $taggedFact = \App\Models\TaggedFact::whereRaw("fact_id = $fact_id AND tag_id = 1")->first();
+
+        $this->assertEquals(1,$taggedFact->tag_id);
+
+
+
+
 
     }
 
