b46e8afb0a023896a0a2b88c6a4e65ff4c54802b
RichNwankwo <RichardNwankwo@visneta.com>
2015-09-03 07:06:34 -0400

    Adds fact relevancy migration

---
+++ database/migrations/2015_09_03_110217_add_relevancy_column.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelevancyColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fact', function(Blueprint $table){
+
+            $table->integer('relevancy');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('fact', function(Blueprint $table){
+
+            $table->removeColumn('relevancy');
+
+        });
+    }
+}
