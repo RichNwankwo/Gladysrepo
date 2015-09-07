dfe2d14903d78862313d023014f5ad41e4e58d8f
Richard Nwankwo <rich.nwan@gmail.com>
2015-09-07 08:44:37 -0400

    New questions when fact inserted

--- app/Commands/FactInserted.php
+++ app/Commands/FactInserted.php
@@ -38,14 +38,40 @@ class FactInserted implements SelfHandling
         // TODO this should actually be an event
         $fact_id = $this->fact->id;
         $questions =  [
-                ['fact_id' => $fact_id, 'question_title' => 'Can you create a test question from this fact?', 'question_type' => 1],
-                ['fact_id' => $fact_id, 'question_title' => 'Write this fact in your own words', 'question_type' => 2],
-                ['fact_id' => $fact_id, 'question_title' => 'Write a paragraph about the fact and its related implications','question_type' => 3],
-                ['fact_id' => $fact_id, 'question_title' => 'How can you apply this to your day, or life in general?', 'question_type' => 4],
-                ['fact_id' => $fact_id, 'question_title' => 'Can you find a related fact to this fact', 'question_type' => 5],
-                ['fact_id' => $fact_id, 'question_title' => 'Can you give an example in recent new or history related to this fact', 'question_type' => 6],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you create a question from this fact?', 'question_type' => 1],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you Write this idea in your own words?', 'question_type' => 2],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you write a paragraph about this idea and its related implications? Remember to save it!','question_type' => 3],
+                ['fact_id' => $fact_id, 'question_title' => 'How can this idea be used in your life?', 'question_type' => 4],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you find a related idea to this fact?', 'question_type' => 5],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you give an example in recent new or history related to this idea?', 'question_type' => 6],
                 ['fact_id' => $fact_id, 'question_title' => 'What is one key term or phrase that you can later define', 'question_type' => 7],
-                ['fact_id' => $fact_id, 'question_title' => 'How can you take action on this piece of information right this moment?', 'question_type' => 8]
+                ['fact_id' => $fact_id, 'question_title' => 'How can you take action on this idea right this moment?', 'question_type' => 8],
+                ['fact_id' => $fact_id, 'question_title' => 'What would be the potential significance of this idea in the future', 'question_type' => 9],
+                ['fact_id' => $fact_id, 'question_title' => 'What is the significance of this idea in other places of the world', 'question_type' => 10],
+                ['fact_id' => $fact_id, 'question_title' => 'How would you think of this idea if you were not you? If you were a women, white, etc.', 'question_type' => 11],
+                ['fact_id' => $fact_id, 'question_title' => 'Name one person you idolize. How would they interpret and use this idea', 'question_type' => 12],
+                ['fact_id' => $fact_id, 'question_title' => 'What are some ideas that build up this idea ', 'question_type' => 13],
+                ['fact_id' => $fact_id, 'question_title' => 'Name one field you have interest in? How would they take advantage of this idea', 'question_type' => 14],
+                ['fact_id' => $fact_id, 'question_title' => 'How can you use this idea in relation tou your strengths?', 'question_type' => 15],
+                ['fact_id' => $fact_id, 'question_title' => 'Can you prove this idea false?', 'question_type' => 16],
+                ['fact_id' => $fact_id, 'question_title' => 'Is there an opposite to this idea?', 'question_type' => 17],
+                ['fact_id' => $fact_id, 'question_title' => 'What if this idea was in a vacuum and had no constraints? What would be the effect?', 'question_type' => 19],
+                ['fact_id' => $fact_id, 'question_title' => 'What would this idea be link if it exaggerated?', 'question_type' => 20],
+                ['fact_id' => $fact_id, 'question_title' => 'What would this idea be like if it was minimized', 'question_type' => 21],
+                ['fact_id' => $fact_id, 'question_title' => 'How can this idea relate to your goals in life', 'question_type' => 22],
+                ['fact_id' => $fact_id, 'question_title' => 'Is this an idea that would be accepted or rejected by society', 'question_type' => 23],
+                ['fact_id' => $fact_id, 'question_title' => 'What would be the effects if everybody accepted this idea as universal truth', 'question_type' => 24],
+                ['fact_id' => $fact_id, 'question_title' => 'If you discovered this idea on your life, what would it be worth?', 'question_type' => 25],
+                ['fact_id' => $fact_id, 'question_title' => 'How can this idea prepare for future?', 'question_type' => 26],
+                ['fact_id' => $fact_id, 'question_title' => 'How can this idea provide a better world?', 'question_type' => 27],
+                ['fact_id' => $fact_id, 'question_title' => 'How can this idea set you apart from most people', 'question_type' => 28],
+                ['fact_id' => $fact_id, 'question_title' => 'How can remember this idea for future?', 'question_type' => 29],
+                ['fact_id' => $fact_id, 'question_title' => 'Slowly re-read this idea', 'question_type' => 30],
+                ['fact_id' => $fact_id, 'question_title' => 'Defend the merit of this idea.', 'question_type' => 31]
+
+
+
+
 
             ];
         Question::insert($questions);
